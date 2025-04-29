8faf35d6c85be3d3dab7ceb7a0b50d1f9a62cbbc
jayOnTheStreet <taychakhun.r@gmail.com>
2025-04-29 09:23:20 +0700

    feat: add question-3 answer

--- question-3.js
+++ question-3.js
@@ -1,20 +1,21 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: ฟังก์ชัน findStudentById เป็นสับเซตของ O(n) เนื่องจากฟังก์ชันจะทำงานไม่ช้าไปกว่า n รอบในกรณีที่มี input มาก ๆ
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: ฟังก์ชัน findProductPrice เป็นซับเซตของ O(log n)
+เนื่องจากฟังก์ชันจะทำงานไม่ช้าไปกว่า log n รอบในกรณีที่มี input มาก ๆ
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
-
+Answer: ฟังก์ชัน findProductPrice มีประสิทธิภาพมากกว่า หากอ้างอิงตามจำนวน input ที่รับเข้ามามาก ๆ เมื่อเรานำไปวาดกราฟระหว่าง `จำนวนรอบในการทำงาน` กับ `จำนวน input`
+จะเห็นได้ว่ามีความแตกต่างอย่างเห็นได้ชัดเมื่อจำนวน input มากขึ้นเรื่อย ๆ
 
 */
